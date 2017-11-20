c2760f7610459052d73062481d90235c14565004
Anna Thompson <anna.thompson.lex@gmail.com>
2017-11-20 16:47:07 +0100

    Added image and ImagePanel class

--- src/FlightBookingApp.java
+++ src/FlightBookingApp.java
@@ -1,22 +1,30 @@
 import java.awt.EventQueue;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 public class FlightBookingApp {
 
 	private JFrame frame;
 
+
 	/**
 	 * Create the application.
 	 */
 	public FlightBookingApp() {
 		initialize();
+		frame.setVisible(true);
 	}
 
 	/**
@@ -26,23 +34,28 @@ public class FlightBookingApp {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 586, 401);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.NORTH);
-		
+
 		JButton btnBooking = new JButton("Book tickets");
-//		btnBooking.addActionListener(e -> new BookTickesFrame()); TODO Change to new class
+		//		btnBooking.addActionListener(e -> new BookTickesFrame()); TODO Change to new class
 		panel.add(btnBooking);
-		
+
 		JButton btnQuery = new JButton("Querry");
-//		btnQuery.addActionListener(e -> new QuerryFrame()); TODO Change to new class
+		//		btnQuery.addActionListener(e -> new QuerryFrame()); TODO Change to new class
 		panel.add(btnQuery);
-		
+
 		JButton btnAdminister = new JButton("Administer");
-//		btnAdminister.addActionListener(e -> new AdminiserFrame()); TODO Change to new class
+		//		btnAdminister.addActionListener(e -> new AdminiserFrame()); TODO Change to new class
 		panel.add(btnAdminister);
+		
+		JPanel panel_1 = new ImagePanel("images/airplane.jpg");
+		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
+
+
 	}
-	
-	
+
+
 
 }
