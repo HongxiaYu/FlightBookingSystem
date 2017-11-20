c100ab276c0b4d99971a22278957ef5830171e64
Anna Thompson <anna.thompson.lex@gmail.com>
2017-11-20 16:09:58 +0100

    Added GUI Frame

--- src/FlightBookingApp.java
+++ src/FlightBookingApp.java
@@ -1,26 +1,16 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FlightBookingApp {
 
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					FlightBookingApp window = new FlightBookingApp();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the application.
@@ -36,6 +26,28 @@ public class FlightBookingApp {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		
+		JButton btnBooking = new JButton("Book");
+		btnBooking.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel.add(btnBooking);
+		
+		JButton btnQuery = new JButton("Query");
+		panel.add(btnQuery);
+		
+		JButton btnAdminister = new JButton("Administer");
+		panel.add(btnAdminister);
+	}
+	
+	private void bookSeat() {
+		int option = JOptionPane.showInternalConfirmDialog(frame,
+				"please choose one", "information",
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
 	}
 
 }
