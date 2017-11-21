90f052c4443525830ccb374cc613c216a07f7da9
Anna Thompson <anna.thompson.lex@gmail.com>
2017-11-21 08:55:25 +0100

    bugfix in ImagePanel class

--- src/ImagePanel.java
+++ src/ImagePanel.java
@@ -2,8 +2,6 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
@@ -22,7 +20,7 @@ public class ImagePanel extends JPanel{
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters            
+        g.drawImage(image, 0, 0, this);          
     }
 
 }
