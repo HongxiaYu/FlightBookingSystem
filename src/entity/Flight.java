3cf660c889c4bb26849430edfa5d627ed8ca2246
Anna Thompson <anna.thompson.lex@gmail.com>
2017-11-21 11:25:33 +0100

    Added Airplane and basic methods in Flight

--- src/entity/Flight.java
+++ src/entity/Flight.java
@@ -1,15 +1,59 @@
 package entity;
 
+import java.util.ArrayList;
+
+import util.GenericIDNext;
+
 public  class Flight {
 	
-	public static int flightId;
-	public static String flightName;
+	private int flightId;
+	private String flightName;
+	private Airplane airplane;
+	private EconomyClassSeat[] economyClassSetas;
+	private FirstClassSeat[] firstClassSeats;
 	
-	
-	
-	public Flight(int flightId,String flightName) {
-		this.flightId=flightId;
+
+	public Flight(String flightName, Airplane airplane) {
+		this.flightId=GenericIDNext.getFlightIDNext();
 		this.flightName=flightName;
+		this.airplane = airplane;
+		this.economyClassSetas = new EconomyClassSeat[airplane.getNumberOfEconomyClassSeats()];
+		
+		this.firstClassSeats = new FirstClassSeat[airplane.getNumberOfFirstCalssSeats()];
 	}
+	
+	private void makeSeats() {
+		for(int i = 0; i < economyClassSetas.length; i++) {
+			economyClassSetas[i] = new EconomyClassSeat(i + 1);
+		}
+		for(int j = 0; j < firstClassSeats.length; j++) {
+			firstClassSeats[j] = new FirstClassSeat(j + 1);
+		}
+	}
+	
+	public int getFlightId() {
+		return flightId;
+	}
+
+
+	public String getFlightName() {
+		return flightName;
+	}
+
+
+	public Airplane getAirplane() {
+		return airplane;
+	}
+
+
+	public EconomyClassSeat[] getEconomyClassSetas() {
+		return economyClassSetas;
+	}
+
+
+	public FirstClassSeat[] getFirstClassSeats() {
+		return firstClassSeats;
+	}
+
 	
 }
