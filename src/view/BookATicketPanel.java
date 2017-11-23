e65c03744bc574e45c7768336d78b26f10d568d0
LenaHongXia <bjyuhongxia@gmail.com>
2017-11-23 10:41:58 +0100

    to get new ticket

--- src/view/BookATicketPanel.java
+++ src/view/BookATicketPanel.java
@@ -1,6 +1,8 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
@@ -8,7 +10,15 @@ import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
+import entity.Customer;
+import entity.FirstClassSeat;
+import entity.Food;
+import entity.Ticket;
+
 public class BookATicketPanel extends JPanel {
+	SeatSelectPanel seatSelectPanel;
+	MealPanel mealSelectPanel;
+	CustomerInfoInputPanel customerInfoPanel;
 
 	public BookATicketPanel(int itype) {
 		initMainPanel(itype);
@@ -16,17 +26,17 @@ public class BookATicketPanel extends JPanel {
 
 	public void initMainPanel(int itype) {
 		this.setLayout(null);
-		SeatSelectPanel seatSelectPanel = new SeatSelectPanel(itype);
+		seatSelectPanel = new SeatSelectPanel(itype);
 		seatSelectPanel.setSize(313, 180);
 		seatSelectPanel.setLocation(0, 0);
 		seatSelectPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
 
-		MealPanel mealSelectPanel = new MealPanel(itype);
+		mealSelectPanel = new MealPanel(itype);
 		mealSelectPanel.setBorder(new TitledBorder("Choose Meal"));
 		mealSelectPanel.setSize(264, 341);
 		mealSelectPanel.setLocation(323, 0);
 
-		CustomerInfoInputPanel customerInfoPanel = new CustomerInfoInputPanel();
+		customerInfoPanel = new CustomerInfoInputPanel();
 		customerInfoPanel.setSize(313, 150);
 		customerInfoPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
 		customerInfoPanel.setLocation(0, 191);
@@ -35,6 +45,14 @@ public class BookATicketPanel extends JPanel {
 		this.add(mealSelectPanel);
 		this.setSize(597, 372);
 
+	}
+	
+	public Ticket getTicket() {		
+		Customer customer = new Customer("John", "Berg", "FirstClass", 1, "pwno22323", "male");
+		FirstClassSeat seat = new FirstClassSeat(1);
+		List<Food> menu = mealSelectPanel.getSelectedFoods().getSelectedFoods();
+		Ticket t1 = new Ticket(customer, seat, menu);		
+		return t1;
 	}
 
 	public static void main(String[] args) {
